7843e19e835c233ba282119b16a3d2d90686c0e1
Carlos Arthur <arthurhollyfild@gmail.com>
2026-03-15 15:58:49 -0300

    Create exercicio02.py

---
+++ exercicio02.py
@@ -0,0 +1,12 @@
+#variáveis
+nome=input("Digite seu nome:")
+n1=int(input("Digite um número:"))
+n2= int(2)
+n3= int(3)
+s= n1*n2
+t=n1*n3
+#codeline
+print("É um prazer te conhecer, {}!".format(nome)) 
+print('O dobro de', n1 , "resulta em {}".format(s))
+print('Já o triplo de', n1, 'resulta em {}'.format(t))
+
